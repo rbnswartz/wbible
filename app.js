f2db40e7d47001c4f3609fd1bc8a94660346d189
Reuben Swartz <rbn.swartz@gmail.com>
2015-05-25 09:56:41 -0400

    Now pulls in information correctly from unfoldingWord

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 angular.module("app",[]);
 angular.module("app").controller("controller",function($scope,$http,$sce){
-    $scope.data="hello world";
+    $scope.index={};
+    $http.get("https://api.unfoldingword.org/uw/txt/2/catalog.json").success(function(data){
+        $scope.index=data;
+    });
+    $scope.getBook=function(book){
+        $http.get(book.src).success(function(data){
+            $scope.bookData = data;
+        });
+    }
 });
