f0f1c14d55f738cc5d55b45e3f6af738db241978
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2015-07-08 12:09:26 -0400

    Added the USFM regex

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ angular.module("app").controller("controller",function($scope,$http,$sce){
         $scope.bookData = "Loading...";
         $http.get(book.src).success(function(data){
             $scope.bookData = data;
+            $scope.lines = $scope.bookData.split("\n");
+            $scope.lines.map(function(data){
+                console.log(parseUSFM(data));
+            });
         });
-    }
+    };
+    $scope.parseUSFM = function(input){
+        var pattern = /^\\(\S+) *(\d*) *(.*)$/;
+        var result = pattern.exec(input);
+        if (result === null){
+            return {
+                tag: null,
+                number: null,
+                text: null 
+            };
+        }else{
+        
+        }
+        return {
+            tag: result[1],
+            number: result[2],
+            text: result[3]
+        };
+    };
 });
